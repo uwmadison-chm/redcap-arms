021aa40f05fdff2929459f90fd143170f7ab1668
Glitch (glitch-hello-website) <none>
2021-10-15 15:44:17 +0000

    👨‍👨‍👦🐸 Checkpoint ./script.js:1984148/1452 ./index.html:1984148/1026

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-"use strict";
+//"use strict";
 
+import { Application, Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
+window.Stimulus = Application.start()
 
+Stimulus.register("hello", class extends Controller {
+  static targets = [ "name" ]
 
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+  connect() {
+    console.log("I connected yo");
+  }
+})
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
 
 // Stuff to turn arrays into reasonable-ish-length strings
 // maybe overkill
